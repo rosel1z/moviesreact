19199381ec1fb2cc5ca707e9dcfcc40984636e04
rosel1z <cayennenovi@yandex.ru>
2025-03-10 21:01:03 +0300

    Добавлены новые файлы

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer className="page-footer light-blue darken-3">
+            <div className="container">
+                © {new Date().getFullYear()} Елизавета Новикова
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <header>
+            <nav className="light-blue darken-3">
+                <div className="nav-wrapper container">
+                    <a href="index.html" className="brand-logo">Кинотеатр</a>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
